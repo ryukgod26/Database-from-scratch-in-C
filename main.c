718efad1e4c5e661e2301d5e78e44f2a6a04825d
root <root@localhost.localdomain>
2026-08-31 04:46:35 +0000

    Changed read_prompt to read_input and changed if statements to switch condition

--- main.c
+++ main.c
@@ -6,21 +6,47 @@ InputBuffer* input = get_input_buffer();
 while(true){
 
 print_prompt();
-read_prompt(input);
+read_input(input);
+
+if(input->buffer[0] == '.'){
+    switch(do_meta_command(input)){
+        case META_COMMAND_SUCCESS:
+            continue;
+        case META_COMMAND_UNRECOGNISED_COMMAND:
+            printf("Uncrecognised Command %s \n" , input->buffer);
+            continue;
+    }
+
+}
+
+Statement statement;
+switch (prepare_statement(input,&statement))
+{
+case PREPARE_SUCCESS:
+    
+    break;
+
+case PREPARE_UNRECOGNISED_COMMAND:
+    printf("Unrecognised Keyword at the start  %s \n",input->buffer);
+    continue;
+}
+
+execute_statement(&statement);
+
+}
+
+// if(strcmp(input->buffer,".exit")==0){
+
+// close_input_buffer(input);
+// return 0;
+
+// }
+// else{
+// printf("Unrecognised Command: %s\n",input->buffer);
+// }
 
 
-if(strcmp(input->buffer,".exit")==0){
 
-close_input_buffer();
 return 0;
-
-}
-else{
-printf("Unrecognised Command: %s.\n",input->buffer);
 }
 
-}
-
-
-return 0;
-}
